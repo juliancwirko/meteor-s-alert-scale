f7846f2c4a08923a19fcb3e70d2d8678a81ef291
Julian Ćwirko <julian.cwirko@gmail.com>
2015-05-25 21:06:35 +0200

    updates

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
     'summary': 'Scale effect for s-alert - simple and fancy notifications / alerts for Meteor.',
-    'version': '1.0.4',
+    'version': '1.0.5',
     'git': 'https://github.com/juliancwirko/meteor-s-alert-scale.git',
     'name': 'juliancwirko:s-alert-scale'
 });
 
 Package.onUse(function (api) {
-    api.use('juliancwirko:s-alert@2.3.3', ['client']);
-    api.imply('juliancwirko:s-alert@2.3.3', ['client']);
+    api.use('juliancwirko:s-alert@2.4.0', ['client']);
+    api.imply('juliancwirko:s-alert@2.4.0', ['client']);
     api.addFiles([
         's-alert-scale.css'
     ], 'client');
